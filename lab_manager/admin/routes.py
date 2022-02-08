2a7b84c9f8fa29db830c141ce82c67d3e05d8317
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-02-08 16:15:26 -0300

    Prepparing admin models

--- lab_manager/admin/routes.py
+++ lab_manager/admin/routes.py
@@ -3,22 +3,22 @@ from lab_manager.users.forms import Registration, Login
 
 admin = Blueprint('admin', __name__)
 
-@admin.route("/register", methods = ['GET', 'POST'])
+@admin.route("/admin/register", methods = ['GET', 'POST'])
 def register():
 
     form = Registration()
     if form.validate_on_submit():
-        flash(f'User {form.username} registered successfully! Go to you profile page and update your personal information.', category='success')
+        flash(f'Admin {form.username} registered successfully! Go to you profile page and update your personal information.', category='success')
         return redirect('/success')
 
     return render_template('register_adm.jinja2', title='Register', form=form)
 
-@admin.route("/login", methods = ['GET', 'POST'])
+@admin.route("/admin/login", methods = ['GET', 'POST'])
 def login():
 
     form = Login()
     if form.validate_on_submit():
-        flash(f'User {form.username} logged in successfully!', category='success')
+        flash(f'Admin {form.username} logged in successfully!', category='success')
         return redirect('/success')
 
     return render_template('login_adm.jinja2', title='Login', form=form)
