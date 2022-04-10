a83bf687f5e0bcf6c5532e65ea5ffe0e9539733d
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-04-10 15:47:37 -0300

    Error Handling - templates better bootstrap

--- lab_manager/errors/errors_handler.py
+++ lab_manager/errors/errors_handler.py
@@ -10,5 +10,6 @@ def notFoundError(error):
 
 @errors.app_errorhandler(500)
 def internalServerError(error):
-    # User app_errorhandler to avoid circular import error
+    # User app_errorhandler to avoid circular import error]
+    db.session.rollback()
     return render_template('errors/500.jinja2', user = current_user), 500
