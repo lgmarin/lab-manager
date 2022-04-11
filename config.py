4a8f0e09d72fdb225e23c0a17674d5603477893f
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-04-10 22:05:51 -0300

    Dcoker Compose Fix Config

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'bf4bd5bd26c6dbefa05f29c1a8f87060'
 
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}/{}'.format(
-        os.getenv('DB_USER', 'flask'),
+        os.getenv('DB_USER', 'lab_manager'),
         os.getenv('DB_PASSWORD', ''),
         os.getenv('DB_HOST', 'mysql'),
-        os.getenv('DB_NAME', 'flask')
+        os.getenv('DB_NAME', 'lab_manager')
     )
-    
+
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
     #     'postgres://', 'postgresql://') or \
     #     'sqlite:///' + os.path.join(basedir, 'database.db')
